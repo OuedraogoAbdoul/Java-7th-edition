7d5c110edddfa591b5e1176a7cea60741dfc748a
uabdoul <uabdoul@users.noreply.github.com>
2017-09-20 22:44:26 -0700

    Add files via upload

---
+++ ch01/cubDisplay.java
@@ -0,0 +1,44 @@
+// Wrirte a program that displays a cubic shape.
+
+//import java.util;
+
+public class cubDisplay {
+
+	public static void main(String[] args) {
+	
+	System.out.println( "       ##################");
+	//System.out.println("");
+	System.out.println( "      #                # #");
+	//System.out.println("");
+	System.out.println( "     #                #  #");
+	//System.out.println("");
+	System.out.println( "   #                 #   #");
+	//System.out.println("");
+	System.out.println( "  #                 #    #");
+	//System.out.println("");
+	System.out.println("  ##################     #");
+	System.out.println("");
+	System.out.println("  #                #     #");
+	System.out.println("");
+	System.out.println("  #                #     #");
+	System.out.println("");
+	System.out.println("  #                #     #");
+	System.out.println("");
+	System.out.println("  #                #     #");
+	System.out.println("");  
+	System.out.println("  #                #     #");
+	System.out.println("");
+	System.out.println("  #                #   #");
+	System.out.println("");
+	System.out.println("  #                # #");
+	System.out.println("");
+	System.out.println( "  ################# ");
+	
+	
+	
+	}
+
+
+
+
+}
